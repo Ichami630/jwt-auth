3b4c14da429e012225697346112aee60bddc8dda
ichami630 <brandonichami@gmail.com>
2025-09-09 15:49:44 +0100

    logina and registration with jwt access and refresh token

--- api/api/api.go
+++ api/api/api.go
@@ -4,24 +4,30 @@ import (
 	"net/http"
 
 	"github.com/Iknite-Space/sqlc-example-api/api/controllers"
-	"github.com/Iknite-Space/sqlc-example-api/db/repo"
+	"github.com/Iknite-Space/sqlc-example-api/api/middlewares"
+	"github.com/Iknite-Space/sqlc-example-api/db/store"
 	"github.com/gin-gonic/gin"
 )
 
 type Router struct {
+	authController *controllers.AuthController
 	userController *controllers.UserController
 }
 
-func NewRouter(q repo.Querier) http.Handler {
+func NewRouter(s store.Store) http.Handler {
 	r := gin.Default()
 
 	//controllers
-	userCtrl := controllers.NewUserController()
+	userCtrl := controllers.NewUserController(s)
+	authCtrl := controllers.NewAuthController(s)
 
 	//public routes
+	r.POST("/login", authCtrl.Login)
+	r.POST("/register", userCtrl.Register)
 
 	//protected routes
 	protected := r.Group("/")
+	protected.Use(middlewares.AuthMiddleware())
 	protected.GET("/profile", userCtrl.Profile)
 
 	return r
--- api/api/controllers/user_controller.go
+++ api/api/controllers/user_controller.go
@@ -3,13 +3,20 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/Iknite-Space/sqlc-example-api/db/repo"
+	"github.com/Iknite-Space/sqlc-example-api/db/store"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type UserController struct{}
+type UserController struct {
+	store store.Store
+}
 
-func NewUserController() *UserController {
-	return &UserController{}
+func NewUserController(store store.Store) *UserController {
+	return &UserController{
+		store: store,
+	}
 }
 
 func (u *UserController) Profile(c *gin.Context) {
@@ -18,4 +25,37 @@ func (u *UserController) Profile(c *gin.Context) {
 		"message": "Welcome to your profile 🎉",
 		"userId":  userId,
 	})
+}
+
+func (u *UserController) Register(c *gin.Context) {
+	var req repo.CreateUserParams
+
+	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//hash the password
+	hashedPwd, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not hash password"})
+		return
+	}
+
+	//create new user
+	user, err := u.store.Do().CreateUser(c, repo.CreateUserParams{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: string(hashedPwd),
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"success": true,
+		"user":    user,
+	})
+
 }
